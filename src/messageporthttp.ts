bbe108cba33d9a6f85147ab37dec6a46ad2c8663
rariariari <mpgomes@berkeley.edu>
2024-07-04 17:16:29 -0700

    Update messageporthttp.ts

--- src/messageporthttp.ts
+++ src/messageporthttp.ts
@@ -310,7 +310,7 @@ async function handleHttpuvRequests(
     // send it to httpuv
     if (!request.more_body) {
       try {
-        const bytes = await new shelter.RRaw(Array.from(body));
+        const bytes = await new shelter.RRaw(body);
         const env = await new shelter.REnvironment({ bytes, appName });
         const httpuvResp = await webRProxy.webR.evalR(
           `
